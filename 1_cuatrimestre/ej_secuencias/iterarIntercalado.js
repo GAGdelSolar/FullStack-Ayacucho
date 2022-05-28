eda8c572254d2e56299b438ea3bf5b5e448a573d
guilledelsolar <guillermogarciadelsolar@gmail.com>
2022-05-28 11:33:25 -0300

    iterar

---
+++ 1_cuatrimestre/ej_secuencias/iterarIntercalado.js
@@ -0,0 +1,12 @@
+var indice = 0;
+var indiceA = 0;
+var indiceB = 0;
+var arregloA = [8, 9, 6];
+var arregloB = [6, 7, 3];
+var arregloC = new Array();
+for (indice = 0, 6, indice++;;) {
+    arregloC[indice] = arregloA[indiceA];
+    indiceA++;
+    arregloC[indice] = arregloB[indiceB];
+    indiceB++;
+}
---
+++ 1_cuatrimestre/ej_secuencias/iterarIntercalado.ts
@@ -0,0 +1,16 @@
+let indice: number = 0;
+let indiceA: number =0;
+let indiceB: number =0;
+
+let arregloA: number [] = [8,9,6];
+let arregloB: number [] = [6,7,3];
+let arregloC:number [] = new Array ()
+
+for (indice=0, 6, indice++){
+    arregloC [indice]= arregloA[indiceA];
+    indiceA++;
+    arregloC [indice] = arregloB[indiceB];
+    indiceB++;
+
+}
+console.log (arregloC);
